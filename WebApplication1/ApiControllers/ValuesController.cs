45516cd9ccb8a90e3e95d63839cecfff5ff56be5
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2018-08-03 14:11:24 -0400

    Add initial action using StreamContent.

---
+++ WebApplication1/ApiControllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web.Http;
+
+namespace WebApplication1.ApiControllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values.
+        [HttpGet]
+        [HttpHead]
+        public HttpResponseMessage Get(
+            int id = 1)
+        {
+            var encoding = new UTF8Encoding(false, true);
+            var ms = new MemoryStream();
+            using (var writer = new StreamWriter(ms, encoding, 8192, true))
+            {
+                var numLines = id;
+                foreach (var line in Enumerable.Range(0, numLines))
+                {
+                    writer.WriteLine("Hey there!");
+                }
+            }
+            ms.Position = 0;
+
+            var response = Request.CreateResponse();
+            response.Content = new StreamContent(ms);
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue($"text/plain")
+            {
+                CharSet = encoding.WebName,
+            };
+            return response;
+        }
+    }
+}
---
+++ WebApplication1/Startup1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(WebApplication1.Startup1))]
+
+namespace WebApplication1
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var httpConfiguration = new HttpConfiguration();
+            httpConfiguration.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new
+            {
+                id = RouteParameter.Optional,
+            });
+
+            app.UseWebApi(httpConfiguration);
+        }
+    }
+}
